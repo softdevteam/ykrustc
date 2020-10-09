92150a831351d7a4c20131830ae23c03663f4560
Edd Barrett <vext01@gmail.com>
2020-10-09 18:11:09 +0100

    Remove stuff we no longer need.

--- compiler/rustc_feature/src/builtin_attrs.rs
+++ compiler/rustc_feature/src/builtin_attrs.rs
@@ -237,11 +237,7 @@ pub const BUILTIN_ATTRIBUTES: &[BuiltinAttribute] = &[
     ungated!(no_mangle, AssumedUsed, template!(Word)),
 
     // Yorick-related stuff.
-    ungated!(trace_head, AssumedUsed, template!(Word)),
-    ungated!(trace_inputs, AssumedUsed, template!(Word)),
-    ungated!(trace_tail, AssumedUsed, template!(Word)),
     ungated!(do_not_trace, AssumedUsed, template!(Word)),
-    ungated!(thread_tracer, AssumedUsed, template!(Word)),
     ungated!(interp_step, AssumedUsed, template!(Word)),
 
     ungated!(used, AssumedUsed, template!(Word)),
